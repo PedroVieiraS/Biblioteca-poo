76facf313477e37e90301f3330b6981982b9e173
PedroVieiraS <pedrocorrea978@gmail.com>
2024-07-02 22:18:41 -0300

    autor

--- src/main/java/com/biblioteca/demo/Autor.java
+++ src/main/java/com/biblioteca/demo/Autor.java
@@ -1,12 +1,34 @@
 package com.biblioteca.demo;
 
 public class Autor {
+    private static int contador = 0;
     private int autor_id;
     private String nome;
 
-    public Autor(){
-        
+    public Autor(String nome){
+        this.nome = nome;
+        autor_id = contador;
+        contador++;
     }
+    
+    public int getAutor_id() {
+        return autor_id;
+    }
+
+    public void setAutor_id(int autor_id) {
+        this.autor_id = autor_id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    
 
 
 }
